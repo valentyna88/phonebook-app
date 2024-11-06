5d3e599edc1a847af35f8d5ac65d4d9c2b902a26
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-06 22:19:03 +0200

    Add operations to auth

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const authInstance = axios.create({
+  baseURL: "https://connections-api.goit.global/",
+});
+
+export const setToken = (token) => {
+  authInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+export const clearToken = () => {
+  authInstance.defaults.headers.common.Authorization = "";
+};
