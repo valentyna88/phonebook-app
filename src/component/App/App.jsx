d0bcdeba4898c04a1eae04b1c53e3270045e5198
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-07 12:21:23 +0200

    Add ContactsPage

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -1,30 +1,5 @@
-import css from "./App.module.css";
-import ContactForm from "../ContactForm/ContactForm";
-import SearchBox from "../SearchBox/SearchBox";
-import ContactList from "../ContactList/ContactList";
-import Loader from "../Loader/Loader";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchContacts } from "../../redux/contactsOps";
-import { selectError, selectIsLoading } from "../../redux/contactsSlice";
-
 const App = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-  return (
-    <div className={css.container}>
-      <h1 className={css.title}>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      {isLoading && !error && <Loader />}
-      <ContactList />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,5 +1,30 @@
+import css from "./App.module.css";
+import ContactForm from "../component/ContactForm/ContactForm";
+import SearchBox from "../component/SearchBox/SearchBox";
+import ContactList from "../component/ContactList/ContactList";
+import Loader from "../component/Loader/Loader";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "../redux/contactsOps";
+import { selectError, selectIsLoading } from "../redux/contactsSlice";
+
 const ContactsPage = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      {isLoading && !error && <Loader />}
+      <ContactList />
+    </div>
+  );
 };
 
 export default ContactsPage;
