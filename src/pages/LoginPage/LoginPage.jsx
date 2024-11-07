14dd2254ffbc6863adbcc9f4663b96317669861a
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-07 23:06:12 +0200

    Add LoginPage markup and validation

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,62 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useDispatch } from "react-redux";
+import { apiLogin } from "../../redux/auth/operations";
+import css from "./LoginPage.module.css";
+import { LoginUserSchema } from "../../utils/schemas";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
 const LoginPage = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, actions) => {
+    console.log("values: ", values);
+    dispatch(apiLogin(values));
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={LoginUserSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={css.form}>
+        <label className={css.label}>
+          <span>Email:</span>
+          <Field
+            type="text"
+            name="email"
+            className={css.input}
+            placeholder="example.email@example.com"
+          />
+          <ErrorMessage
+            className={css.errorMessage}
+            name="email"
+            component="span"
+          />
+        </label>
+        <label className={css.label}>
+          <span>Password:</span>
+          <Field
+            type="password"
+            name="password"
+            className={css.input}
+            placeholder="Enter your password"
+          />
+          <ErrorMessage
+            className={css.errorMessage}
+            name="password"
+            component="span"
+          />
+        </label>
+        <button type="submit">Sign In</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default LoginPage;
--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -12,3 +12,12 @@ export const RegisterUserSchema = Yup.object({
     .min(8, "Password length must be at least 8 characters")
     .required("Password is required"),
 });
+
+export const LoginUserSchema = Yup.object({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password length must be at least 8 characters")
+    .required("Password is required"),
+});
