5aa1edd5ff2ea4ce4435dc318c1a1a409e46dc55
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-11 11:32:10 +0200

    Add Layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { lazy, Suspense, useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { refreshUser } from '../../redux/auth/operations';
 import { selectUserDataIsRefreshing } from '../../redux/auth/slice';
 import PrivateRoute from '../PrivateRoute';
 import RestrictedRoute from '../RestrictedRoute';
-import Navigation from '../Navigation/Navigation';
 import Loader from '../Loader/Loader';
+import Layout from '../Layout';
 import css from './App.module.css';
 
 const HomePage = lazy(() => import('../../pages/HomePage'));
@@ -27,26 +27,23 @@ const App = () => {
   return isRefreshing ? (
     <Loader />
   ) : (
-    <div className={css.container}>
-      <Navigation />
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/contacts"
-            element={<PrivateRoute component={<ContactsPage />} />}
-          />
-          <Route
-            path="/register"
-            element={<RestrictedRoute component={<RegistrationPage />} />}
-          />
-          <Route
-            path="/login"
-            element={<RestrictedRoute component={<LoginPage />} />}
-          />
-        </Routes>
-      </Suspense>
-    </div>
+    <Layout className={css.container}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/contacts"
+          element={<PrivateRoute component={<ContactsPage />} />}
+        />
+        <Route
+          path="/register"
+          element={<RestrictedRoute component={<RegistrationPage />} />}
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute component={<LoginPage />} />}
+        />
+      </Routes>
+    </Layout>
   );
 };
 
